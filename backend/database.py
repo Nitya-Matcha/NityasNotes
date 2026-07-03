a50a0053211da5b800ced31d2ec97c957d77c9e9
Nitya Matcha <nitya.matcha@gmail.com>
2026-07-03 16:38:18 -0400

    removed dotov

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,9 @@
-from dotenv import load_dotenv
+
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-load_dotenv()  # MUST be at the top before getenv
+
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
